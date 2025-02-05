fd5950cdb9318fc7b7a8b605c65ee418d35d275e
sawyer bristol <sawyerbristol@gmail.com>
2025-02-05 15:09:52 -0700

    fixup temp server
    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(generic_const_exprs)]
 #![cfg_attr(not(test), no_std, no_main)]
 // #![warn(missing_docs)]
 
@@ -40,84 +41,72 @@ impl Default for CodecId {
     }
 }
 
-const MAX: usize = 3;
+const MAX_SERVICES: usize = 3;
 
-pub fn create_server<'a, const ATT_MTU: usize, M: RawMutex>(
-    name_id: &'a [u8; 2],
+pub async fn run_server<'a, const ATT_MTU: usize, M: RawMutex>(
+    conn: &Connection<'_>,
+    name_id: &str,
     appearance: &'a [u8; 2],
-) -> AttributeServer<'a, M, MAX> {
-    let mut table: AttributeTable<'_, M, MAX> = AttributeTable::new();
+    storage: &'a mut [u8; MAX_SERVICES * ATT_MTU],
+) {
+    let mut table: AttributeTable<'_, M, MAX_SERVICES> = AttributeTable::new();
     let mut svc = table.add_service(Service::new(0x1800u16));
-    let _ = svc.add_characteristic_ro(0x2a00u16, name_id);
+    let _ = svc.add_characteristic_ro(0x2a00u16, &name_id);
     let _ = svc.add_characteristic_ro(0x2a01u16, appearance);
     svc.build();
 
     // Generic attribute service (mandatory)
     table.add_service(Service::new(0x1801u16));
 
-    let mut s1: Vec<u8, 200> = Vec::new();
-    let mut s2: Vec<u8, 200> = Vec::new();
-    pacs::PacsServer::<ATT_MTU>::new(
+    let mut storages = storage.chunks_exact_mut(ATT_MTU);
+    let pacs = pacs::PacsServer::<ATT_MTU>::new(
         &mut table,
         None,
         None,
         None,
         None,
-        (Default::default(), s1.as_mut_slice()),
-        (Default::default(), s2.as_mut_slice()),
+        (Default::default(), storages.next().unwrap()),
+        (Default::default(), storages.next().unwrap()),
     );
 
-    AttributeServer::<M, MAX>::new(table)
+    let server = AttributeServer::<M, MAX_SERVICES>::new(table);
+
+    loop {
+        match conn.next().await {
+            ConnectionEvent::Disconnected { reason } => {
+                #[cfg(feature = "defmt")]
+                info!("[gatt] disconnected: {:?}", reason);
+                break;
+            }
+            ConnectionEvent::Gatt { data } => match data.process(&server).await {
+                Ok(data) => {
+                    if let Some(event) = data {
+                        if let Some(resp) = pacs.handle(&event) {
+                            if let Err(err) = resp {
+                                event.reject(err).unwrap()
+                            } else {
+                                event.accept().unwrap()
+                            };
+                        } else {
+                            #[cfg(feature = "defmt")]
+                            warn!("There was no known handler to handle this event")
+                        }
+                    }
+                }
+                Err(e) => {
+                    #[cfg(feature = "defmt")]
+                    warn!("[gatt] error processing event: {:?}", e);
+                }
+            },
+        }
+    }
 }
 
-// // TODO: Create macro and to derive traits for each different service
-// // This will work for v0.0.1
-// #[gatt_server]
-// // Ideally this could be implemented by the user and include other services like battery etc.
-// // Until that can easily be done it will be implemented here, for easy event processing
-// pub struct LeAudioServer {
-//     // pub pacs: pacs::PACS,
-//     // pub cats: CATS<5>,
-//     // pacs: pacs::PacsSource,
-// }
-
-// pub async fn run_client<C: Controller, const L2CAP_MTU: usize>(
-//     client: &GattClient<'_, C, 10, L2CAP_MTU>,
-// ) {
-//     select(client.task(), async {
-//         // pacs::sink_client(&client)
-//     })
-//     .await;
-// }
-
-// pub async fn run_server(server: &LeAudioServer<'_>, conn: &Connection<'_>) {
-//     loop {
-//         match conn.next().await {
-//             ConnectionEvent::Disconnected { reason } => {
-//                 #[cfg(feature = "defmt")]
-//                 info!("[gatt] disconnected: {:?}", reason);
-//                 break;
-//             }
-//             ConnectionEvent::Gatt { data } => match data.process(&server).await {
-//                 Ok(data) => {
-//                     if let Some(event) = data {
-//                         if let Some(resp) = pacs::try_handle_event(&server.pacs, &event) {
-//                             if let Err(err) = resp {
-//                                 event.reject(err).unwrap()
-//                             } else {
-//                                 event.accept().unwrap()
-//                             };
-//                         } else {
-//                             #[cfg(feature = "defmt")]
-//                             warn!("There was no known handler to handle this event")
-//                         }
-//                     }
-//                 }
-//                 Err(e) => {
-//                     #[cfg(feature = "defmt")]
-//                     warn!("[gatt] error processing event: {:?}", e);
-//                 }
-//             },
-//         }
-//     }
-// }
+pub async fn run_client<C: Controller, const L2CAP_MTU: usize>(
+    client: &GattClient<'_, C, 10, L2CAP_MTU>,
+) {
+    select(client.task(), async {
+        // pacs::sink_client(&client)
+    })
+    .await;
+}
